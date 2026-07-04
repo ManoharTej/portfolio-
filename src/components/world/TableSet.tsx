f9fd2e399d3dcd547f1e6c98fe4cde1828da1740
Manohar Tej <manohartejkandula@gmail.com>
2026-07-04 20:20:34 +0530

    Move contact card closer to the camera so it is fully visible

--- src/components/world/TableSet.tsx
+++ src/components/world/TableSet.tsx
@@ -27,7 +27,7 @@ const ContactCard = () => {
   return (
     <mesh 
       ref={cardRef} 
-      position={[0.2, 0.962, 0.2]} 
+      position={[-0.2, 0.962, 0]} 
       rotation={[0, -0.1, 0]}
       scale={[0, 0, 0]}
       onClick={(e) => {
