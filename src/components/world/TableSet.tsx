3cf63649f646f23da251e3d62ff44c23253c9eeb
Manohar Tej <manohartejkandula@gmail.com>
2026-07-06 19:59:46 +0530

    UI: Move contact card to be exactly 10cm in front of DB avatar

--- src/components/world/TableSet.tsx
+++ src/components/world/TableSet.tsx
@@ -27,7 +27,7 @@ const ContactCard = () => {
   return (
     <mesh 
       ref={cardRef} 
-      position={[0.2, 0.962, 0.2]} 
+      position={[0.25, 0.962, -0.5]} 
       rotation={[0, -0.1, 0]}
       scale={[0, 0, 0]}
       onClick={(e) => {
